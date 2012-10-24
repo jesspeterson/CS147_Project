1e4f13888918ee5e25e32da59af40c2b1c4c5e32
Sam Gerstenzang <samgerstenzang@gmail.com>
2012-10-24 16:15:22 -0700

    got rid of mobilesafari chrome

--- header.php
+++ header.php
@@ -7,6 +7,8 @@
 	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
 	<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
 	<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
 </head> 
 
 
