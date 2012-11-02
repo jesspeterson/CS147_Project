72a23cd10ad0c2b450d0e5bbf42bf0849cf50c8c
Sam Gerstenzang <samgerstenzang@gmail.com>
2012-11-01 22:04:59 -0700

    update javascript for slider.

--- index.php
+++ index.php
@@ -94,6 +94,22 @@
 			}
 		
 		}
+		
+		var slider, bullets;
+		$(document).ready(function(){
+		 	slider = new Swipe(document.getElementById('slider'), {
+	      	callback: function(e, pos) {
+
+	        var i = bullets.length;
+	        while (i--) {
+	          bullets[i].className = ' ';
+	        }
+	        bullets[pos].className = 'on';
+
+	      }
+	    }),
+	    bullets = document.getElementById('position').getElementsByTagName('em');
+		});
 	}
 	
 	setupMap();
