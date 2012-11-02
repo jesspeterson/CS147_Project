2f4f2f2e58eb261644c2794c6059392990b7854f
Sam Gerstenzang <samgerstenzang@gmail.com>
2012-11-01 20:17:55 -0700

    fixed merge conflict.

--- index.php
+++ index.php
@@ -106,16 +106,22 @@ function setupMap() {
 <div id="slider" class="swipe">
 	<ul>
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> fixed merge conflict.
 		<li style="display:block;"><div><a href="fact.php?id=666"><p>Completed in 1941, <strong>Hoover Tower</strong> is the tallest building on the Stanford campus at 285 feet.</p></a></div></li>
 		<li style="display:none;"><div><a href="fact.php?id=667">Former Secretary of State <strong>Condoleeza Rice</strong> has an office on the tenth floor of Hoover Tower.</a></div></li>
 		<li style="display:none;"><div><a href="fact.php?id=668">The <strong>Hoover Institution</strong>, a conservate public policy think tank, is housed partly in Hoover Tower.</a></div></li>
 		<li style="display:none;"><div><a href="fact.php?id=669">Dissident Russian writer <strong>Aleksandr Solzhenitsyn</strong> lived on the 11th floor of Hoover Tower while exiled.</a></div></li>
+<<<<<<< HEAD
 =======
 		<li style="display:block;"><div><h2><a href="fact.php">Hoover Tower</a></h2><p>C</p></div></li>
 		<li style="display:none;"><div><h2><a href="fact.php">Condoleeza Rice</a></h2></div></li>
 		<li style="display:none;"><div><h2><a href="fact.php">Hoover Institution</a></h2></div></li>
 		<li style="display:none;"><div><h2><a href="fact.php">Herbert Hoover</a></h2></div></li>
 >>>>>>> facts now grouped by addresses. slider thing now almost works.
+=======
+>>>>>>> fixed merge conflict.
 	</ul>
 </div>
 
