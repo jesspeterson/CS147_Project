d84ceedfce69dde2c7dfe887ff6883dd11c10bd0
Sam Gerstenzang <samgerstenzang@gmail.com>
2012-11-28 22:52:07 -0800

    Added character count on add page

--- B/add.php
+++ B/add.php
@@ -17,6 +17,19 @@
 window.optimizely = window.optimizely || [];
 window.optimizely.push(['trackEvent', 'added_fact']);
 </script>
+
+<p id="factguide">The quality of the facts you enter is what makes WikiTour great. Make sure your fact is a positive and interesting contribution to the WikiTour community.</p>
+<form id="addfact" name="addfact" action="submitfact.php" method="post" data-ajax="false">
+	<input type="hidden" name="sourcepage" value="<?php echo str_replace("_", " ", $source); ?>" />
+	<textarea name="facttext" maxlength="120" placeholder="Enter your fact&mdash;why will visitors here want to learn about <?php echo $source; ?>?"></textarea>
+	<input type="hidden" name="latitude" value="">
+	<input type="hidden" name="longitude" value="">
+	<input type="hidden" name="address" value="">
+	<input type="button" value="Pin fact to my location" onclick="setLoc();"/>
+</form>
+<div class="navbutton">
+	<a href="index.html" data-role="button" data-rel="back">Cancel</a>
+</div>
 <script>
 
 	var lat = 0.0;
@@ -35,9 +48,7 @@ window.optimizely.push(['trackEvent', 'added_fact']);
 		lat = position.coords.latitude.toFixed(7);
 		lng = position.coords.longitude.toFixed(7);	
 		
-		console.log(lat+","+lng);
 		$.getJSON("revgeocode.php?latlng="+lat+","+lng, function(data) {
-		console.log(data);
 		address = data.results[0].formatted_address;
 	});			
 	}
@@ -50,20 +61,11 @@ window.optimizely.push(['trackEvent', 'added_fact']);
     	document.addfact.address.value = address;
     	document.forms["addfact"].submit();
     }
+
+	$('#facttext').keyup(function() {
+		console.log(1);
+		console.log($('#facttext').value().length);
+	});
 </script>
-
-<p id="factguide">The quality of the facts you enter is what makes WikiTour great. Make sure your fact is a positive and interesting contribution to the WikiTour community.</p>
-<form id="addfact" name="addfact" action="submitfact.php" method="post" data-ajax="false">
-	<input type="hidden" name="sourcepage" value="<?php echo str_replace("_", " ", $source); ?>" />
-	<textarea name="facttext" maxlength="120" placeholder="Enter your fact&mdash;why will visitors here want to learn about <?php echo $source; ?>?"></textarea>
-	<input type="hidden" name="latitude" value="">
-	<input type="hidden" name="longitude" value="">
-	<input type="hidden" name="address" value="">
-	<input type="button" value="Pin fact to my location" onclick="setLoc();"/>
-</form>
-<div class="navbutton">
-	<a href="index.html" data-role="button" data-rel="back">Cancel</a>
-</div>
-
 
 <?php include('footer.php'); ?>
--- add.php
+++ add.php
@@ -11,11 +11,26 @@
 	include('header.php');
 	$source = $_GET["source"];
 ?>
+
 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBwiHi6BAeRu7z44MIb8VTAxeyVe7WLvjo&sensor=true"></script>
 <script>
 window.optimizely = window.optimizely || [];
 window.optimizely.push(['trackEvent', 'added_fact']);
 </script>
+
+<p id="factguide">The quality of the facts you enter is what makes WikiTour great. Make sure your fact is a positive and interesting contribution to the WikiTour community.</p>
+<form id="addfact" name="addfact" action="submitfact.php" method="post" data-ajax="false">
+	<input type="hidden" name="sourcepage" value="<?php echo str_replace("_", " ", $source); ?>" />
+	<span id="character_limit" style="float:right;">140</span>
+	<textarea name="facttext" id="facttext" maxlength="140" placeholder="Enter your fact&mdash;why will visitors here want to learn about <?php echo $source; ?>?"></textarea>
+	<input type="hidden" name="latitude" value="">
+	<input type="hidden" name="longitude" value="">
+	<input type="hidden" name="address" value="">
+	<input type="button" value="Pin fact to my location" onclick="setLoc();"/>
+</form>
+<div class="navbutton">
+	<a href="index.html" data-role="button" data-rel="back">Cancel</a>
+</div>
 <script>
 
 	var lat = 0.0;
@@ -34,9 +49,7 @@ window.optimizely.push(['trackEvent', 'added_fact']);
 		lat = position.coords.latitude.toFixed(7);
 		lng = position.coords.longitude.toFixed(7);	
 		
-		console.log(lat+","+lng);
 		$.getJSON("revgeocode.php?latlng="+lat+","+lng, function(data) {
-		console.log(data);
 		address = data.results[0].formatted_address;
 	});			
 	}
@@ -49,20 +62,10 @@ window.optimizely.push(['trackEvent', 'added_fact']);
     	document.addfact.address.value = address;
     	document.forms["addfact"].submit();
     }
+
+	$('#facttext').keydown(function() {
+		$("#character_limit").html(140 - $('#facttext').val().length);
+	});
 </script>
-
-<p id="factguide">The quality of the facts you enter is what makes WikiTour great. Make sure your fact is a positive and interesting contribution to the WikiTour community.</p>
-<form id="addfact" name="addfact" action="submitfact.php" method="post" data-ajax="false">
-	<input type="hidden" name="sourcepage" value="<?php echo str_replace("_", " ", $source); ?>" />
-	<textarea name="facttext" maxlength="120" placeholder="Enter your fact&mdash;why will visitors here want to learn about <?php echo $source; ?>?"></textarea>
-	<input type="hidden" name="latitude" value="">
-	<input type="hidden" name="longitude" value="">
-	<input type="hidden" name="address" value="">
-	<input type="button" value="Pin fact to my location" onclick="setLoc();"/>
-</form>
-<div class="navbutton">
-	<a href="index.html" data-role="button" data-rel="back">Cancel</a>
-</div>
-
 
 <?php include('footer.php'); ?>
